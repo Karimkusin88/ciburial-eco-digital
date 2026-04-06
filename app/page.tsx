c5604487ee1fc8aa011ffeafd243e9ad944d830a
Karimkusin88 <159538866+Karimkusin88@users.noreply.github.com>
2026-04-06 14:12:44 +0000

    fix: mobile layout hero & navbar

--- app/page.tsx
+++ app/page.tsx
@@ -215,8 +215,8 @@ export default function Home() {
         .ac.op{max-height:1200px;}
 
         /* mobile menu */
-        .mob{top: 70px;left: 0;width: 100%;background: var(--cw);opacity:0;transform:translateY(-10px);pointer-events:none;transition:opacity .25s,transform .25s cubic-bezier(.22,1,.36,1);}
-        .mob.op{opacity:1;transform:translateY(0);pointer-events:all;}
+        .mob{top:70px;left:0;width:100%;background: var(--cw);opacity:0;transform:translateY(-10px);pointer-events:none;transition:opacity .25s,transform .25s cubic-bezier(.22,1,.36,1);}
+        .mob.op{opacity:1;visibility:visible;transform:translateY(0);pointer-events:all;}
 
         /* kegiatan card */
         .kc{transition:transform .3s cubic-bezier(.22,1,.36,1),box-shadow .3s ease;}
